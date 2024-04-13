d95bdbecd89ca43f31ab9411675be6368c58c9f8
Vinay Pant <vinaypant24@gmail.com>
2024-04-13 20:31:48 +0530

    Skeleton Code

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-import sys
-
-
-from tkinter import messagebox
-
-import tkinter as tk 
+import tkinter as tk
 from tkinter import ttk
 import customtkinter as ctk
 import ttkbootstrap as ttk
 
 
 
+main_window  = tk.Tk()
 
+
+
+main_window.mainloop()
