31a409f515c9d4f7bc65214799b29fe7e8711f01
Vinay Pant <vinaypant24@gmail.com>
2024-04-15 20:35:39 +0530

    Basic App - No Controls

--- main.py
+++ main.py
@@ -4,9 +4,29 @@ import customtkinter as ctk
 import ttkbootstrap as ttk
 
 
+from PIL import Image, ImageTk
 
-main_window  = tk.Tk()
+ctk.set_appearance_mode("dark")  # Modes: system (default), light, dark
+ctk.set_default_color_theme("blue") 
+
+
+# Setting main app along with the title and the iconf or the app : 
+main_window  = ctk.CTk()
+main_window.title("Batch Downloader")
+# Setting up the icon later : 
+
+# Setting default app size for the main app : 
+main_window.geometry("800x600")
+main_window.resizable(False , False)
+
+# main_window.wm_attributes('-toolwindow', True) # This makes the simple titleabr with only the close button and no minimize button 
 
 
 
+
+
+
+
+
+# Running the main app: 
 main_window.mainloop()
