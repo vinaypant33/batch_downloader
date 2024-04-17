468f2f6e76afa8269584ddc58f2b660dfa7e49c6
Vinay Pant <vinaypant24@gmail.com>
2024-04-17 17:18:11 +0530

    skeleton code

--- main.py
+++ main.py
@@ -19,17 +19,29 @@ main_window.title("Batch Downloader")
 main_window.geometry("800x600")
 main_window.resizable(False , False)
 
+
+## Constatns for the current window : 
+WIDTH = 600
+HEIGHT = main_window.winfo_height()
+
+
+
+
 # main_window.wm_attributes('-toolwindow', True) # This makes the simple titleabr with only the close button and no minimize button 
 
 
 ### Defining the controls : 
-website_url  = ctk.CTkEntry(master=main_window)
-scrap_button  = ctk.CTkButton(master = main_window , text="Start Scrapping")
+upper_panel  = tk.PanedWindow(master = main_window , width=WIDTH)
+website_url  = ctk.CTkEntry(master=upper_panel)
+scrap_button  = ctk.CTkButton(master = upper_panel , text="Start Scrapping")
+
+middle_panel  =tk.PanedWindow(master=main_window , width=WIDTH , background="green")
 
 
 
 
 ### Placing the Controls : 
+upper_panel.pack(side="top" , expand='y', padx=2 , pady=2)
 website_url.pack()
 scrap_button.pack()
 
