c26335e21ce074c205d04d406ba82e30f6bbbc93
Vinay Pant <vinaypant24@gmail.com>
2024-04-20 09:36:49 +0530

    UI - ttkbookstrap

---
+++ bootstrapttk.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+
+main_app = tk.Tk()
+
+
+# Setting up the controls for the main app : 
+
+
+
+
+
+
+
+
+
+
+
+
+
+main_app.mainloop()
